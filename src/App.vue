1face63e7a33ba1f61e4b22e543bd92b18ea44d8
Spencer Alexander <slalexander92@gmail.com>
2018-08-30 22:19:52 -0700

    add front-end skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
   <div id="app">
-    <UploadManager />
+    <div class="manager-container">
+      <div class="wrapper">
+        <!-- <UploadForm /> -->
+        <InputForm
+          v-bind:inputs='inputs'
+          v-bind:errors='errors'
+        />
+      </div>
+    </div>
+
+    <p>{{ inputs.item }}</p>
   </div>
 </template>
 
 <script>
 
 // load components here
-import UploadManager from './components/UploadManager.vue'
+import InputForm from './components/InputForm.vue'
+
 
 export default {
   name: 'app',
   components: {
-    UploadManager
-  }
+    InputForm
+  },
+  data(){
+
+    return{
+      photos:[],
+      inputs:{
+        item: '',
+        description:'',
+        price: '',
+      },
+      errors:{
+        item: '',
+        description:'',
+        price: '',
+      }
+
+    };
+  },
+
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import './globals/_variables.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,4 +57,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.manager-container{
+  margin: auto;
+  width: 700px;
+  background-color: #fafafa;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.1);
+
+  .wrapper{
+    padding: 20px;
+  }
+
+}
+
 </style>
